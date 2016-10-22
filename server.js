f472e03cfe8c1a32a828153c76c3493c4db20544
Nayanex <holanda.nayana@gmail.com>
2016-10-22 15:50:17 -0200

    Connect4 Game
    Prototype of Simple Connect4 Game using the javascript framework PIXI.js

---
+++ server.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var app = express();
+
+app.use(express.static('public'));
+
+app.get('/hello.html', function (req, res) {
+   res.sendFile( __dirname + "/" + "hello.html" );
+})
+
+app.get('/index.html', function (req, res) {
+   res.sendFile( __dirname + "/" + "index.html" );
+})
+
+var server = app.listen(8081, function () {
+
+  var host = server.address().address
+  var port = server.address().port
+
+  console.log("Example app listening at http://%s:%s", host, port)
+
+})
